5f8d845c5939447bcf35f06910ee9a37758efddf
cwangg897 <dhkdrb897@gmail.com>
2024-06-03 14:42:05 +0900

    Step Prac

---
+++ src/main/java/com/example/springbatchincation/job/StepNextJobConfiguration.java
@@ -0,0 +1,58 @@
+package com.example.springbatchincation.job;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class StepNextJobConfiguration {
+
+
+    @Bean
+    public Job stepNextJob(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new JobBuilder("stepNextJob", jobRepository)
+                .start(step1(jobRepository, transactionManager))
+                .next(step2(jobRepository, transactionManager))
+                .build();
+    }
+
+    @Bean
+    public Step step1(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("step1", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println(">>>>> This is Step1");
+                    return RepeatStatus.FINISHED;
+                }, transactionManager).build();
+    }
+
+    @Bean
+    public Step step2(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("step2", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println(">>>> This is Step2");
+                    return RepeatStatus.FINISHED;
+                }, transactionManager).build();
+    }
+
+    @Bean
+    public Step step3(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("step3", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println(">>>> This is Step3");
+                    return RepeatStatus.FINISHED;
+                }, transactionManager).build();
+    }
+
+
+}
